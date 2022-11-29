a822cf32556ee8afa4d7ca62cbf180a9f5101540
davi <davipinheiro1993@gmail.com>
2022-11-29 09:29:32 -0300

    Add Home

--- costs/src/components/pages/Home.jsx
+++ costs/src/components/pages/Home.jsx
@@ -1,5 +1,19 @@
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+import styles from './Home.module.css'
+
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>
+                Bem-vindo ao <span>Costs</span>
+            </h1>
+            <p>Comece a gerenciar seus projetos agora mesmo!</p>
+            <LinkButton to='/newproject' text={'Criar Projeto'}></LinkButton>
+            <img src={savings} alt="Costs"/>
+        </section>
+    )
 }
 
 export default Home
