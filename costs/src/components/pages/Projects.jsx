3f01218680b2715039166763df1e09a2fc4940a6
davi <davipinheiro1993@gmail.com>
2022-12-02 11:53:16 -0300

    Add classes to Projects

--- costs/src/components/pages/Projects.jsx
+++ costs/src/components/pages/Projects.jsx
@@ -1,5 +1,8 @@
 import { useLocation } from "react-router-dom"
 import Message from "../layout/Message"
+import styles from "./Projects.module.css"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
 
 function Projects() {
     const location = useLocation();
@@ -9,9 +12,15 @@ function Projects() {
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton text={"Novo Projeto"}></LinkButton>
+            </div>
             {message && <Message msg={message} type={"success"}/> }
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
