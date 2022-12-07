d7f0ad2a5d62ffb3af321809b031663bd5811e8a
davi <davipinheiro1993@gmail.com>
2022-12-07 11:29:26 -0300

    Add styles to edit-form

--- costs/src/components/pages/Project.jsx
+++ costs/src/components/pages/Project.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 import styles from './Project.module.css'
+import Loading from '../layout/Loading'
+import Container from '../layout/Container'
 
 function Project() {
     const {id} = useParams();
     const [project, setProject] = useState(); 
+    const [showProjectForm, setShowProjectForm] = useState(false);
 
     useEffect(() => {
         fetch(`http://localhost:5000/projects/${id}`, {
@@ -17,7 +20,43 @@ function Project() {
         .catch(err => console.log(err))
     }, [id])
 
-    return <p>Projeto</p>
+    function toggleProjectForm() {
+        setShowProjectForm(!showProjectForm)
+    }
+
+    return (
+        <>
+            {project ? 
+            <div className={styles.project_details}>
+                <Container customClass="column">
+                    <div className={styles.details_container}>
+                        <h1>Projeto: {project.name}</h1>
+                        <button className={styles.btn} onClick={toggleProjectForm}>
+                            {!showProjectForm ? 'Editar projeto': 'Fechar'}
+                        </button>
+                        {!showProjectForm ? (
+                            <div className={styles.project_info}>
+                                <p>
+                                    <span>Categoria:</span> {project.category.name}
+                                </p>
+                                <p>
+                                    <span>Total de orçamento:</span> R${project.budget}
+                                </p>
+                                <p>
+                                    <span>Total utilizado:</span> R${project.costs}
+                                </p>
+                            </div>
+                        ) : (
+                            <div className={styles.project_info}>
+                                <p>formulário</p>
+                            </div>
+                        )}
+                    </div>
+                </Container>
+            </div>
+            : <Loading/>}
+        </>
+    )
 }
 
 export default Project
