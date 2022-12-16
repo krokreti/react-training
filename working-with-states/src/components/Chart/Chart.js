f65cad7dfe9277a6bfc625790c617e15ec21d630
davi <davipinheiro1993@gmail.com>
2022-12-16 11:56:46 -0300

    Finish chart project

--- working-with-states/src/components/Chart/Chart.js
+++ working-with-states/src/components/Chart/Chart.js
@@ -3,10 +3,14 @@ import ChartBar from "./ChartBar";
 import './Chart.css';
 
 function Chart(props) {
+    const dataPointValue = props.dataPoints.map(dataPoint => dataPoint.value)
+    // o spread esta tirando todos os elemendos do array e colocando como se fossem valores individuais
+    const totalMaximum = Math.max(...dataPointValue);
+
     return (
         <div className="chart">
             {props.dataPoints.map(dataPoint => (
-                <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label} />
+                <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label} />
             ))}
         </div>)
 }
--- working-with-states/src/components/Expenses/Expenses.js
+++ working-with-states/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-
 import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -21,6 +21,7 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
