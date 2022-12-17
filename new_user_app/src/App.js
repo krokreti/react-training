e0e6e4122bf48d80671743a9ced8aec5b91c10ab
Davi Pinheiro <davipinheiro1993@gmail.com>
2022-12-17 10:18:56 -0300

    Initial practice commit

---
+++ new_user_app/src/App.js
@@ -0,0 +1,14 @@
+import Card from "./components/Card/Card";
+import styles from './App.module.css'
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <Card>
+        Teste
+      </Card>
+    </div>
+  );
+}
+
+export default App;
