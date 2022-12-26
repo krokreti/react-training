95017671b329df05f14a8efc6c5b9ec6a34af6b1
Davi Pinheiro <davipinheiro1993@gmail.com>
2022-12-26 17:21:22 -0300

    Fallback state

--- star-wars-app/src/App.js
+++ star-wars-app/src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   function fetchMoviesHandler() {
+    setIsLoading(true)
     fetch('https://swapi.dev/api/films/').then(
       response => {
         return response.json();
@@ -21,8 +23,23 @@ function App() {
         }
       })
       setMovies(transformedMovies);
+      setIsLoading(false)
     })
     .catch();
+  }
+
+  async function fetchMoviesHandlerByAsync() {
+    const response = await fetch('https://swapi.dev/api/films/');
+    const data = await response.json();
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }
+    })
+    setMovies(transformedMovies)
   }
 
   return (
@@ -31,7 +48,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
