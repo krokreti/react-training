fca1f390f5af61e3bc3c1a704afd4483a0638cab
Davi Pinheiro <davipinheiro1993@gmail.com>
2022-12-27 20:06:06 -0300

    Add project to form validation

---
+++ complex-forms/src/components/SimpleInput.js
@@ -0,0 +1,50 @@
+import { useRef, useState } from "react";
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value);
+  }
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    if (enteredName.trim() == '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+
+    console.log(enteredName)
+    const enteredValue = nameInputRef.current.value
+    console.log("🚀 ~ file: SimpleInput.js:16 ~ formSubmissionHandler ~ enteredValue", enteredValue)
+
+    setEnteredName('')
+  };
+
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input 
+          type='text' 
+          id='name' 
+          onChange={nameInputChangeHandler} 
+          ref={nameInputRef}
+        />
+        {!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
