fcfd5357c632beebfcbad3d1e1a5d572bb8ff934
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-01-02 20:24:56 -0300

    Add Context to handle authentication

--- authentication/src/components/Profile/ProfileForm.js
+++ authentication/src/components/Profile/ProfileForm.js
@@ -1,8 +1,15 @@
+import { useRef } from 'react';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+
+  const submitHandler = event => {
+    event.preventDefault();
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
         <input type='password' id='new-password' />
