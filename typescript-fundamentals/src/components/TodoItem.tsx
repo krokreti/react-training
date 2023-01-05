f76168016200c04228a944e9805231d3784b3048
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-01-05 19:05:23 -0300

    remove item using bind

--- typescript-fundamentals/src/components/TodoItem.tsx
+++ typescript-fundamentals/src/components/TodoItem.tsx
@@ -1,7 +1,7 @@
 import styles from './TodoItem.module.css'
 // const TodoItem: React.FC<{ todo: Todo }> = (props) => {
-const TodoItem: React.FC<{ text: string }> = (props) => {
-    return <li className={styles.item}>{props.text}</li>
+const TodoItem: React.FC<{ text: string, id: string, removeItem: () => void }> = (props) => {
+    return <li className={styles.item} onClick={props.removeItem}>{props.text}</li>
 };
 
 export default TodoItem;
--- typescript-fundamentals/src/components/Todos.tsx
+++ typescript-fundamentals/src/components/Todos.tsx
@@ -3,10 +3,11 @@ import TodoItem from "./TodoItem"
 import styles from './Todos.module.css'
 
 // const Todos: React.FC<{ items: string[] }> = (props) => {
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[], onRemoveTodo: (id: string) => void }> = (props) => {
+
     return (<ul className={styles.todos}>
         {props.items.map(item => 
-            (<TodoItem key={item.id} text={item.text}/>)
+            (<TodoItem key={item.id} text={item.text} id={item.id} removeItem={props.onRemoveTodo.bind(null, item.id)}/>)
         )}
     </ul>
     )
