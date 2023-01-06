e4e92e21cb85dc420865dc178f959ddd2065e553
Davi Pinheiro <davipinheiro1993@gmail.com>
2023-01-06 09:50:08 -0300

    Add 3 different tests

--- react-tests/src/components/Greeting.test.js
+++ react-tests/src/components/Greeting.test.js
@@ -1,13 +1,53 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
-test('renders Hello World as a text', () => {
-    // Arrange
-    render(<Greeting/>)
 
-    //Act
-    // nothing
+//describe define um suite
+describe('Greeting component', () => {
 
-    // Asset
-    const helloWorldElement = screen.getByText('Hello World', {exact: false})
-    expect(helloWorldElement).toBeInTheDocument();
-});
+    test('renders Hello World as a text', () => {
+        // Arrange
+        render(<Greeting/>)
+    
+        //Act
+        // nothing
+    
+        // Asset
+        const helloWorldElement = screen.getByText('Hello World', {exact: false})
+        expect(helloWorldElement).toBeInTheDocument();
+    });
+
+    test('renders good to se you if the button was no clicked', () => {
+        render(<Greeting/>)
+
+        const paragraphElement = screen.getByText('It\'s good to see you', {exact: false})
+        expect(paragraphElement).toBeInTheDocument();
+    });
+
+    test('renders Changed if the button was clicked', () => {
+        render(<Greeting/>)
+
+        //Act
+        const buttonElement = screen.getByRole('button')
+        userEvent.click(buttonElement)
+
+        //Assert
+        const outputElement = screen.getByText('Changed!');
+        expect(outputElement).toBeInTheDocument();
+    })
+
+    test('does not render good to see you if the button was clicked', () => {
+        //Arrange
+        render(<Greeting/>)
+
+        //Act
+        const buttonElement = screen.getByRole('button')
+        userEvent.click(buttonElement)
+
+        //Assert
+        const paragraphElement = screen.queryByText('good to see you', {exact: false});
+        expect(paragraphElement).toBeNull();
+    })
+
+})
+
