b7d1f26f9da946e7efbd5d3fc6402337107b0f7f
davi <davipinheiro1993@gmail.com>
2023-01-30 10:52:56 -0300

    Updated with getStaticPaths

--- next-features/pages/[meetupId]/index.js
+++ next-features/pages/[meetupId]/index.js
@@ -10,4 +10,40 @@ const MeetupDetails = () => {
     );
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: 'm1'
+                },
+            },
+            {
+                params: {
+                    meetupId: 'm2'
+                }
+            }
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+    //fetch the data for a single meetup
+
+    const meetupId = context.params.meetupId;
+
+    return {
+        props: {
+            meetupData: {
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png",
+                title: "First meetup",
+                address: "Some street, some city",
+                description: "This is a first meetup",
+                id: meetupId,
+            }
+        }
+    }
+}
+
 export default MeetupDetails;
